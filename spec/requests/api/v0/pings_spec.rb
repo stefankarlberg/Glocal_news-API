1b2edccfd163831f9054e320a737ef9bb36b074c
zanenkn <zane.neikena@gmail.com>
2019-05-23 10:06:39 +0200

    Adds a json_response helper method

--- spec/requests/api/v0/pings_spec.rb
+++ spec/requests/api/v0/pings_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Api::V0::PingsController, type: :request do
     it 'should return Pong' do
       get '/api/v0/pings'
 
-      json_response = JSON.parse(response.body)
+
       expect(response.status).to eq 200
       expect(json_response['message']).to eq 'Pong'
     end
