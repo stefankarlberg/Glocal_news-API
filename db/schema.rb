f7936b467bca7c6c60d0284ca1b39069954f027a
zanenkn <zane.neikena@gmail.com>
2019-05-28 17:01:02 +0200

    Generates Review model with db columns 'score' and 'comment' and an association to the Article model, fixes a FactoryBot for the Review model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_081230) do
+ActiveRecord::Schema.define(version: 2019_05_28_145420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,4 +25,14 @@ ActiveRecord::Schema.define(version: 2019_05_24_081230) do
     t.string "written_by"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "score"
+    t.string "comment"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_reviews_on_article_id"
+  end
+
+  add_foreign_key "reviews", "articles"
 end
