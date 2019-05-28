4a84ccd17dc58e9db7c9f4c4e5fa9059a4db450c
raptorf1 <raptor_f1@hotmail.com>
2019-05-28 20:14:27 +0200

    Configures serializers so that every articles GET request contains the category as a response

--- app/serializers/articles/show_serializer.rb
+++ app/serializers/articles/show_serializer.rb
@@ -1,3 +1,4 @@
 class Articles::ShowSerializer < ActiveModel::Serializer
-  attributes :id, :title, :ingress, :body, :image, :created_at, :written_by, :category_id
+  attributes :id, :title, :ingress, :body, :image, :created_at, :written_by
+  belongs_to :category, serializer: Categories::IndexSerializer
 end
