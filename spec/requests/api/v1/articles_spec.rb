6c7b568102e8c7b9991fb7ea50d5ecd243ae248b
raptorf1 <raptor_f1@hotmail.com>
2019-05-28 15:51:31 +0200

    Fixes tests after merge conflicts. Edits index action of Articles controller cause the serializer was not working previously.

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,4 +1,3 @@
-
 RSpec.describe Api::V1::ArticlesController, type: :request do
   let(:headers) { { HTTP_ACCEPT: 'application/json' } }
 
@@ -9,7 +8,7 @@ RSpec.describe Api::V1::ArticlesController, type: :request do
 
     it 'returns a collection of articles' do
       get '/api/v1/articles', headers: headers
-      expect(json_response['data'].count).to eq 5
+      expect(json_response.count).to eq 5
     end
 
     it 'returns 200 response' do
@@ -34,38 +33,48 @@ RSpec.describe Api::V1::ArticlesController, type: :request do
   end
 
   describe 'POST /api/v1/articles' do
-    let(:category) { FactoryBot.create(:category) }
-    it 'creates an article entry' do
-      post '/api/v1/articles', params: {
-        article: { 
-          title: 'Gothenburg is great', 
-          ingress: 'According to many', 
-          body: 'Not many people really think that Stockholm is a better place to live in', 
-          image: 'https://assets.craftacademy.se/images/people/students_group.png',
-          written_by: 'Steffe Karlberg',
-          category_id: category.id
-        } 
-      }, headers: headers
-      expect(json_response['message']).to eq 'Successfully created'
-      expect(response.status).to eq 200
-    end
-  
-    it 'can not be created without all fields filled in' do
-      post '/api/v1/articles', params: {
-        article: {
-          title: "Stockolm is not too bad",
-          written_by: 'Steffe Karlberg',
-        }
-      }, headers: headers
+    describe 'successfully' do
+      let(:category) { FactoryBot.create(:category) }
       
-      expect(json_response['error']).to eq ["Category must exist", "Ingress can't be blank", "Body can't be blank", "Image can't be blank", "Category can't be blank"]
-      expect(response.status).to eq 422
-      end
+      before do
+        post '/api/v1/articles', params: {
+          article: {
+            title: 'Gothenburg is great',
+            ingress: 'According to many',
+            body: 'Not many people really think that Stockholm is a better place to live in',
+            image: 'https://assets.craftacademy.se/images/people/students_group.png',
+            written_by: 'Steffe Karlberg',
+            category_id: category.id
+            }
+        }, headers: headers
+        end
 
-      it 'send back into the response the newly created article information' do
-        article = Article.last
-        expect(json_response['article_id']).to eq article.id
-      end
+        it 'creates an article entry' do
+          expect(json_response['message']).to eq 'Successfully created'
+          expect(response.status).to eq 200
+        end
 
+        it 'sends back into the response the newly created article information' do
+          article = Article.last
+          expect(json_response['article_id']).to eq article.id
+        end  
+    end
+  end
+
+  describe 'POST /api/v1/articles' do
+    describe 'unsuccessfully' do
+
+      it 'can not be created without all fields filled in' do
+        post '/api/v1/articles', params: {
+          article: {
+            title: "Stockolm is not too bad",
+            written_by: 'Steffe Karlberg',
+          }
+        }, headers: headers
+
+        expect(json_response['error']).to eq ["Category must exist", "Ingress can't be blank", "Body can't be blank", "Image can't be blank", "Category can't be blank"]
+        expect(response.status).to eq 422
+      end
+    end
   end
 end
