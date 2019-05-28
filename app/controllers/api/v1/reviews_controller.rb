d1d94a8c556c8810b7b6604a7e5d8e8e75b02934
zanenkn <zane.neikena@gmail.com>
2019-05-28 21:18:17 +0200

    Adds a json response that returns message, review_id, score and comment if the review is created successfully, adjusts and refactors the request spec for Review.

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::ReviewsController < ApplicationController
 
   def create
-    @article = Article.find(params[:article_id])
-    review = @article.reviews.create(review_params)
+    article = Article.find(params[:article_id])
+    review = article.reviews.create(review_params)
+    if review.persisted?
+      render json: { message: 'Successfully created', review_id: review.id, score: review.score, comment: review.comment }
+    else
+      render json: { error: review.errors.full_messages }
+    end
   end
   
   private
--- spec/requests/api/v1/reviews_spec.rb
+++ spec/requests/api/v1/reviews_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Api::V1::ReviewsController, type: :request do
     describe 'successfully' do
       before do
         article = FactoryBot.create(:article)
-        post "/api/v1/articles/"+"#{article.id}"+"/reviews/"+"#{review.id}", params: {
+        post "/api/v1/articles/"+"#{article.id}"+"/reviews", params: {
           review: { 
             score: 8, 
             comment: "Good article, well done!"
@@ -15,19 +15,26 @@ RSpec.describe Api::V1::ReviewsController, type: :request do
         }, headers: headers
       end
 
-      # it 'creates a review entry' do
-      #   expect(json_response['message']).to eq 'Successfully created'
-      # end
-
-      it 'sends back the newly created review as a response ' do
-        article = Article.last
-        review = article.review.last
-        expect(json_response.body).to eq [{"score": "8", "comment": "Good article, well done!"}]
+      it 'creates a review entry' do
+        expect(json_response['message']).to eq 'Successfully created'
       end
 
+      it 'sends back the newly created reviews score as a response ' do
+        article = Article.last
+        review = Review.find_by article_id: article.id
+        expect(json_response['score']).to eq 8
+      end
+
+      it 'sends back the newly created reviews comment as a response ' do
+        article = Article.last
+        review = Review.find_by article_id: article.id
+        expect(json_response['comment']).to eq "Good article, well done!"
+      end
+
+      #Ask Oliver about how to refactor this in order to make sense out of this
       it 'attaches the review to the correct article' do
         article = Article.last
-        review = article.review.last
+        review = Review.find_by article_id: article.id
         expect(review[:article_id]).to eq article.id
       end
     end
@@ -35,12 +42,10 @@ RSpec.describe Api::V1::ReviewsController, type: :request do
 
   describe 'POST /api/v1/articles/id/reviews/id' do
     describe 'unsuccessfully' do
-      before do
-        article = FactoryBot.create(:article)
-      end
 
       it 'can not be created without providing both, score and comment' do
-        post "/api/v1/articles/"+"#{article.id}"+"/reviews/"+"#{review.id}", params: {
+        article = FactoryBot.create(:article)
+        post "/api/v1/articles/"+"#{article.id}"+"/reviews", params: {
           review: {
             score: 8
          }
